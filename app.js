49b72c786cb081c542c95d3b34074b74ab31f500
kartiks2000 <46376692+kartiks2000@users.noreply.github.com>
2020-08-24 23:51:17 +0530

    Add files via upload

--- app.js
+++ app.js
@@ -19,8 +19,12 @@ app.use((req,res,next)=>{
 
 app.use((req,res,next)=>{
     console.log("hell this is middleware function 3");
-    // next() transfer/pass the request to the next middleware function
-    next();
+    
+    // We NOT always have to call next() we could also send a response instead and in express we dont need to use setHeader() or write() to send response using -
+
+    res.send('<h1>Hello this is my response!!</h1>');
+
+    // we can send anything inside it like html, text, file.
 });
 
 const server = http.createServer(app);
